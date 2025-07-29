fdb3c9621a03d0987f46bdb80d26651872764e56
iamkapilll <kapildevchaudhary067@gmail.com>
2025-07-29 20:30:50 +0545

    job done for LandingScreen.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css"
+import "./index.css"
+import { LoadingScreen } from './components/LoadingScreen'
+
+
+const App = () => {
+  return (
+    <>
+    <LoadingScreen />
+    </>
+  )
+}
+
+export default App

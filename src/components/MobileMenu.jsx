b131d96b6e1ec440a087bbebf81d4a09ef6f4992
iamkapilll <kapildevchaudhary067@gmail.com>
2025-07-30 19:02:56 +0545

    updated the MobileMenu links

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -10,7 +10,26 @@ export const MobileMenu = ({menuOpen, setMenuOpen}) =>{
             : "h-0 opacity-0 pointer-events-none"}`}>
 
                 <button onClick={() => setMenuOpen(false)} 
-                className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer">&times;</button>
+                className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer"
+                aria-label="Close Menu"
+                >&times;</button>
+
+                     <a href="#home" 
+                     onClick={() => setMenuOpen(false)}
+                    className="text-gray-300 hover:text-white transition-colors">Home</a>
+
+                     <a href="#about" 
+                     onClick={() => setMenuOpen(false)}
+                    className="text-gray-300 hover:text-white transition-colors">About</a>
+
+                     <a href="#project" 
+                     onClick={() => setMenuOpen(false)}
+                    className="text-gray-300 hover:text-white transition-colors">Projects</a>
+
+                     <a href="#contact" 
+                     onClick={() => setMenuOpen(false)}
+                    className="text-gray-300 hover:text-white transition-colors">Contact</a>
+
             </div>
  
     )

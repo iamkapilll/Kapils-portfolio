c0c39bac2d516e8f554b019583a9c59558950ceb
iamkapilll <kapildevchaudhary067@gmail.com>
2025-07-31 05:56:54 +0545

    started doing stuffs in About component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { LoadingScreen } from './components/LoadingScreen'
 import {Navbar} from './components/Navbar'
 import { MobileMenu } from './components/MobileMenu'
 import {Home} from "./components/sections/Home"
+import { About } from './components/sections/About'
+
 
 
 const App = () => {
@@ -23,6 +25,7 @@ const App = () => {
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
+        <About />
 
       </div>
     </>
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -3,7 +3,7 @@ export const Home = () =>{
         <section id="home"
          className="min-h-screen flex items-center justify-center relative">
             <div className="text-center z-10 px-4">
-                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 
+                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 
                     bg-clip-text text-transparent leading-right">
                     Hi, I'm Kapil Dev
                 </h1>

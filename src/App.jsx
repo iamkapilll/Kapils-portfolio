bf7fa1b69d51cb3eda7414587f4483e21a028c4a
iamkapilll <kapildevchaudhary067@gmail.com>
2025-07-31 05:49:00 +0545

    Finished Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./index.css"
 import { LoadingScreen } from './components/LoadingScreen'
 import {Navbar} from './components/Navbar'
 import { MobileMenu } from './components/MobileMenu'
+import {Home} from "./components/sections/Home"
 
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
 
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
 
       </div>
     </>

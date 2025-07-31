3e4e0858c89e50acef57366cd6f70eadf6064d74
iamkapilll <kapildevchaudhary067@gmail.com>
2025-07-31 22:06:20 +0545

    Backend skills added

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -33,10 +33,22 @@ export const About = () =>{
                                     </span>
                                 ))}
                             </div>
-
                         </div>
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">Backend</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {backendSkills.map((tech, key) =>(
+                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
+                <div></div>
 
             </div>
         </section>

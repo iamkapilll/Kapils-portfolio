410b0832014109ef127496ad1f66a03e4e69c150
iamkapilll <kapildevchaudhary067@gmail.com>
2025-07-31 21:55:37 +0545

    Frontend skills added

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,4 +1,17 @@
 export const About = () =>{
+    const frontendSkills = ["React", 
+        "JavaScript", 
+        "TypeScript", 
+        "TailwindCSS"
+    ]
+
+    const backendSkills = [
+        "Node.js",
+        "Express.js",
+        "MongoDB"
+    ]
+        
+    
     return (
         <section id="about" className="min-h-screen flex items-center justify-center py-20">
             <div className="max-w-3xl mx-auto px-4">
@@ -12,7 +25,14 @@ export const About = () =>{
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                         <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                             <h3 className="text-xl font-bold mb-4">Frontend</h3>
-                            <div className="flex flex-wrap gap-2"></div>
+                            <div className="flex flex-wrap gap-2">
+                                {frontendSkills.map((tech, key) =>(
+                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
 
                         </div>
                     </div>

b98b7b35860fdf7e2c2d03e0bb560bbec8e26e3f
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-02 18:23:23 +0545

    RevealOnScroll added on About.jsx

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,5 @@
+import { RevealOnScroll } from "../RevealOnScroll"
+
 export const About = () =>{
     const frontendSkills = ["React", 
         "JavaScript", 
@@ -14,6 +16,9 @@ export const About = () =>{
 
     return (
         <section id="about" className="min-h-screen flex items-center justify-center py-20">
+
+            <RevealOnScroll>
+
             <div className="max-w-3xl mx-auto px-4">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"
                 > About Me</h2>
@@ -72,6 +77,9 @@ export const About = () =>{
                 </div>
 
             </div>
+
+            </RevealOnScroll>
+
         </section>
     )
 }

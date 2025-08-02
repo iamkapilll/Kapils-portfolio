229a67d5bdbc1ca43119b4f38fec9bc745e38d03
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-02 18:24:49 +0545

    RevealOnScroll added on project.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,8 +1,13 @@
+import { RevealOnScroll } from "../RevealOnScroll"
+
 export const Projects = () => {
     return (
         <section
             id="projects"
             className="min-h-screen flex items-center justify-center py-20">
+
+            <RevealOnScroll>
+
             <div className="max-w-5xl mx-auto px-4">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                     Featured Projects
@@ -57,6 +62,8 @@ export const Projects = () => {
 
                 </div>
             </div>
+
+            </RevealOnScroll>
 
         </section>
     )

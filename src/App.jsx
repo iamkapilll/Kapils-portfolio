8cbaa83a2e459c937c68fe411d0073b1d42db406
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-02 20:42:52 +0545

    The last change

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { MobileMenu } from './components/MobileMenu'
 import {Home} from "./components/sections/Home"
 import { About } from './components/sections/About'
 import {Projects} from './components/sections/Projects'
+import { Contact } from './components/sections/Contact'
 
 
 const App = () => {
@@ -27,6 +28,7 @@ const App = () => {
         <Home />
         <About />
         <Projects />
+        <Contact />
 
       </div>
     </>
